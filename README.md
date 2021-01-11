e286966a5ddd675f64deb3d0ae50d987599306a3
Tim <timothy.seifert@tufts.edu>
2021-01-11 14:11:33 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-A personal website about myself using wrongakram's tutorial and website as a template
---
+++ precache-manifest.241cdd416acce6e8df694a378a961a30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d40467e91f43cbd7e443425522fac6",
+    "url": "/PersonalWebsiteV2/index.html"
+  },
+  {
+    "revision": "58f2731b2333ecc5a935",
+    "url": "/PersonalWebsiteV2/static/css/main.77ceffa4.chunk.css"
+  },
+  {
+    "revision": "3c05e3eac940b65d94f5",
+    "url": "/PersonalWebsiteV2/static/js/2.067b12fc.chunk.js"
+  },
+  {
+    "revision": "5554a16f24e9f12eab3e2315cdb5ff7d",
+    "url": "/PersonalWebsiteV2/static/js/2.067b12fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f2731b2333ecc5a935",
+    "url": "/PersonalWebsiteV2/static/js/main.40f41630.chunk.js"
+  },
+  {
+    "revision": "492d694de7ac2991fe06",
+    "url": "/PersonalWebsiteV2/static/js/runtime-main.4e21d496.js"
+  }
+]);
